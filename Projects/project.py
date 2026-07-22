5163fb2a7c4577827dec3f3c2b6e7162164202e9
Kalyani6662 <kalyanikondapalli0@gmail.com>
2026-07-22 06:06:21 +0530

    Add files via upload

---
+++ Projects/project.py
@@ -0,0 +1,34 @@
+#ATM APPLICATION
+'''while True:
+    amount=100000
+    card=input("insert the card")
+    password=1234
+    if card=="c":
+        print("Welcome kalyani")
+        pwd=int(input("Enter the password"))
+        if pwd==password:
+            print("Select an option")
+            print("1.Balance enquiry")
+            print("2.With draw")
+            option=int(input())
+            if option==1:
+                print("Account balance is",amount)
+            elif option==2:
+                withdraw=int(input("with draw amount"))
+                balance=withdraw-amount
+                print("your account rem balance is",balance)
+            else:
+                print("invalid option") 
+        else:
+            print("Incorrect password")
+    else:
+        print("incorrect card")'''
+        
+                
+                
+            
+            
+
+    
+        
+    
